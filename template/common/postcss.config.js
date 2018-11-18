09377ac5ba4af723673fc5a40ceca9a3587e271b
fxy060608 <fxy060608@gmail.com>
2018-11-18 16:57:03 +0800

    remove postcss.config.js

--- template/common/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const pkg = require('./package.json')
-module.exports = {
-	plugins: [
-		require("autoprefixer")({
-			browsers: pkg.browserslist
-		}),
-		// require("./packages/@uni-app/postcss")
-	]
-}
