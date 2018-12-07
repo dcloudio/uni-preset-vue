9aa478db8edfb00b8427c626997cd4e0f1f70df3
fxy060608 <fxy060608@gmail.com>
2018-12-07 17:08:11 +0800

    升级支持mp-baidu,mp-alipay,mp-weixin

--- generator.js
+++ generator.js
@@ -33,12 +33,29 @@ module.exports = (api, options, rootOptions) => {
 		delete pkg.postcss
 		delete pkg.browserslist
 		return {
+			scripts: {
+				'serve': 'npm run dev:h5',
+				'build': 'npm run build:h5',
+				'dev:h5': 'cross-env NODE_ENV=development UNI_PLATFORM=h5 vue-cli-service uni-serve',
+				'build:h5': 'cross-env NODE_ENV=production UNI_PLATFORM=h5 vue-cli-service uni-build',
+				'dev:mp-weixin': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-weixin vue-cli-service uni-build --watch',
+				'dev:mp-baidu': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-baidu vue-cli-service uni-build --watch',
+				'dev:mp-alipay': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-alipay vue-cli-service uni-build --watch',
+				'build:mp-weixin': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-weixin vue-cli-service uni-build',
+				'build:mp-baidu': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-baidu vue-cli-service uni-build',
+				'build:mp-alipay': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-alipay vue-cli-service uni-build'
+			},
 			dependencies: {
 				'flyio': '^0.6.2',
 				'vuex': '^3.0.1',
-				'@dcloudio/uni-h5': '*'
+				'megalo': '^0.5.4',
+				'@dcloudio/uni-h5': '*',
+				'@dcloudio/uni-mp-weixin': '*',
+				'@dcloudio/uni-mp-baidu': '*',
+				'@dcloudio/uni-mp-alipay': '*'
 			},
 			devDependencies: {
+				'cross-env': '^5.2.0',
 				'@dcloudio/vue-cli-plugin-uni': '*'
 			},
 			browserslist: [
