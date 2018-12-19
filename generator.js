6371ecb55c177c9ea2e6c86212e9e8312a9da8ea
fxy060608 <fxy060608@gmail.com>
2018-12-19 11:31:08 +0800

    test generator

--- generator.js
+++ generator.js
@@ -29,40 +29,40 @@ async function generate(dir, files, base = '') {
 
 module.exports = (api, options, rootOptions) => {
 
-	api.extendPackage(pkg => {
-		delete pkg.postcss
-		delete pkg.browserslist
-		return {
-			scripts: {
-				'serve': 'npm run dev:h5',
-				'build': 'npm run build:h5',
-				'dev:h5': 'cross-env NODE_ENV=development UNI_PLATFORM=h5 vue-cli-service uni-serve',
-				'build:h5': 'cross-env NODE_ENV=production UNI_PLATFORM=h5 vue-cli-service uni-build',
-				'dev:mp-weixin': 'cross-env NODE_ENV=development UNI_PLATFORM=mp-weixin vue-cli-service uni-build --watch',
-				'dev:mp-baidu': 'cross-env NODE_ENV=development UNI_PLATFORM=mp-baidu vue-cli-service uni-build --watch',
-				'dev:mp-alipay': 'cross-env NODE_ENV=development UNI_PLATFORM=mp-alipay vue-cli-service uni-build --watch',
-				'build:mp-weixin': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-weixin vue-cli-service uni-build',
-				'build:mp-baidu': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-baidu vue-cli-service uni-build',
-				'build:mp-alipay': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-alipay vue-cli-service uni-build'
-			},
-			dependencies: {
-				'flyio': '^0.6.2',
-				'vuex': '^3.0.1',
-				'@dcloudio/uni-h5': '*',
-				'@dcloudio/uni-mp-weixin': '*',
-				'@dcloudio/uni-mp-baidu': '*',
-				'@dcloudio/uni-mp-alipay': '*'
-			},
-			devDependencies: {
-				'@dcloudio/vue-cli-plugin-uni': '*'
-			},
-			browserslist: [
-				'last 3 versions',
-				'Android >= 4.4',
-				'ios >= 8'
-			]
-		}
-	})
+	// 	api.extendPackage(pkg => {
+	// 		delete pkg.postcss
+	// 		delete pkg.browserslist
+	// 		return {
+	// 			scripts: {
+	// 				'serve': 'npm run dev:h5',
+	// 				'build': 'npm run build:h5',
+	// 				'dev:h5': 'cross-env NODE_ENV=development UNI_PLATFORM=h5 vue-cli-service uni-serve',
+	// 				'build:h5': 'cross-env NODE_ENV=production UNI_PLATFORM=h5 vue-cli-service uni-build',
+	// 				'dev:mp-weixin': 'cross-env NODE_ENV=development UNI_PLATFORM=mp-weixin vue-cli-service uni-build --watch',
+	// 				'dev:mp-baidu': 'cross-env NODE_ENV=development UNI_PLATFORM=mp-baidu vue-cli-service uni-build --watch',
+	// 				'dev:mp-alipay': 'cross-env NODE_ENV=development UNI_PLATFORM=mp-alipay vue-cli-service uni-build --watch',
+	// 				'build:mp-weixin': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-weixin vue-cli-service uni-build',
+	// 				'build:mp-baidu': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-baidu vue-cli-service uni-build',
+	// 				'build:mp-alipay': 'cross-env NODE_ENV=production UNI_PLATFORM=mp-alipay vue-cli-service uni-build'
+	// 			},
+	// 			dependencies: {
+	// 				'flyio': '^0.6.2',
+	// 				'vuex': '^3.0.1',
+	// 				'@dcloudio/uni-h5': '*',
+	// 				'@dcloudio/uni-mp-weixin': '*',
+	// 				'@dcloudio/uni-mp-baidu': '*',
+	// 				'@dcloudio/uni-mp-alipay': '*'
+	// 			},
+	// 			devDependencies: {
+	// 				'@dcloudio/vue-cli-plugin-uni': '*'
+	// 			},
+	// 			browserslist: [
+	// 				'last 3 versions',
+	// 				'Android >= 4.4',
+	// 				'ios >= 8'
+	// 			]
+	// 		}
+	// 	})
 
 	api.render(async function(files) {
 
