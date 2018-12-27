eb01916a22f9080ffd1d317f6b1ddce325f314a3
fxy060608 <fxy060608@gmail.com>
2018-12-27 18:17:20 +0800

    feat: add babel-plugin-import

--- template/common/babel.config.js
+++ template/common/babel.config.js
@@ -1,7 +1,24 @@
+const plugins = []
+process.UNI_LIBRARIES = process.UNI_LIBRARIES || ['uni-ui']
+process.UNI_LIBRARIES.forEach(libraryName => {
+    plugins.push([
+        'import',
+        {
+            'libraryName': libraryName,
+            'customName': (name) => {
+                return `${libraryName}/lib/${name}/${name}`
+            }
+        }
+    ])
+})
 module.exports = {
-	presets: [
-		['@vue/app', {
-			useBuiltIns: 'entry'
-		}]
-	]
+    presets: [
+        [
+            '@vue/app',
+            {
+                useBuiltIns: 'entry'
+            }
+        ]
+    ],
+    plugins
 }
