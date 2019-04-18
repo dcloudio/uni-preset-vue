208e772afd0a0aeab37e968db6209ed6c9473eca
xiaoyucoding <xiaoyucoding@126.com>
2019-04-18 14:20:14 +0800

    update: 更新默认的应用模板

--- template/default/pages/index/index.vue
+++ template/default/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
 	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
+		<image class="logo" src="/static/logo.png"></image>
 		<view>
-            <text class="title">{{title}}</text>
-        </view>
+			<text class="title">{{title}}</text>
+		</view>
 	</view>
 </template>
 
@@ -28,11 +28,13 @@
 		text-align: center;
 		height: 400upx;
 	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
+
+	.logo {
+		height: 200upx;
+		width: 200upx;
+		margin-top: 200upx;
+	}
+
 	.title {
 		font-size: 36upx;
 		color: #8f8f94;
