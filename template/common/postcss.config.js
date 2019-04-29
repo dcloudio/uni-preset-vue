7d5bddcc24f4439b1e4ba5c2b10f75db64d672db
fxy060608 <fxy060608@gmail.com>
2019-04-29 19:01:22 +0800

    fix(autoprefixer): add remove:false

--- template/common/postcss.config.js
+++ template/common/postcss.config.js
@@ -3,7 +3,8 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('autoprefixer')({
-      browsers: pkg.browserslist
+      browsers: pkg.browserslist,
+      remove:false
     }),
     require('@dcloudio/vue-cli-plugin-uni/packages/postcss')
   ]
