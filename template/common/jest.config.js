abccf448d7bb4ce8e2dd1bcba7b2b37823741aeb
fxy060608 <fxy060608@gmail.com>
2020-06-15 11:30:10 +0800

    refactor(jest): testMatch

--- template/common/jest.config.js
+++ template/common/jest.config.js
@@ -9,6 +9,6 @@ module.exports = {
   watchPathIgnorePatterns: ['/node_modules/', '/dist/', '/.git/'],
   moduleFileExtensions: ['js', 'json'],
   rootDir: __dirname,
-  testMatch: ['<rootDir>/src/__tests__/**/*spec.[jt]s?(x)'],
+  testMatch: ['<rootDir>/src/**/*test.[jt]s?(x)'],
   testPathIgnorePatterns: ['/node_modules/']
 }
