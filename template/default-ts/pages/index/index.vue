ad85c249fcba2b5768330b5097c5b96ed7d3c2d9
wangyaqi <wangyaqi988@163.com>
2020-07-15 11:54:36 +0800

    chore: upx --> rpx

--- template/default-ts/pages/index/index.vue
+++ template/default-ts/pages/index/index.vue
@@ -26,16 +26,25 @@
 
 <style>
 	.content {
-		text-align: center;
-		height: 400upx;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
-    .logo{
-        height: 200upx;
-        width: 200upx;
-        margin-top: 200upx;
-    }
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin: 200rpx auto 50rpx auto;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
 	.title {
-		font-size: 36upx;
+		font-size: 36rpx;
 		color: #8f8f94;
 	}
 </style>
