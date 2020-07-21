c6fe22c01714b7f1671e90a24531f315fa49f9ba
fxy060608 <fxy060608@gmail.com>
2020-07-21 15:11:33 +0800

    feat(vue3): App.vue

--- template/default/App.vue
+++ template/default/App.vue
@@ -1,17 +1,18 @@
 <script>
-	export default {
-		onLaunch: function() {
-			console.log('App Launch')
-		},
-		onShow: function() {
-			console.log('App Show')
-		},
-		onHide: function() {
-			console.log('App Hide')
-		}
-	}
+    import {
+        onLaunch,
+        onShow,
+        onHide
+    } from 'vue'
+    export default {
+        setup() {
+            onLaunch(() => console.log('App Launch'))
+            onShow(() => console.log('App Show'))
+            onHide(() => console.log('App Hide'))
+        }
+    }
 </script>
 
 <style>
-	/*每个页面公共css */
+    /*每个页面公共css */
 </style>
