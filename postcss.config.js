f9791f7cbe9d85b55899a0382fa0e4bc77d390bb
fxy060608 <fxy060608@gmail.com>
2021-11-09 20:50:40 +0800

    chore: update package.json

--- postcss.config.js
+++ postcss.config.js
@@ -4,19 +4,7 @@ const {
 } = require('@dcloudio/uni-cli-shared')
 module.exports = {
   plugins: [
-    uniPostcssPlugin(
-      Object.assign(
-        {
-          page:
-            process.env.UNI_PLATFORM === 'h5'
-              ? 'uni-page-body'
-              : process.env.UNI_PLATFORM === 'app'
-              ? 'body'
-              : ''
-        },
-        parseRpx2UnitOnce(process.env.UNI_INPUT_DIR, process.env.UNI_PLATFORM)
-      )
-    ),
+    uniPostcssPlugin(),
     require('autoprefixer')()
   ]
 }
