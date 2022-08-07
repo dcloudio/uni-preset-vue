eb921a233082a53c5c694824aea3f53a2c26d4a9
zhenyuWang <13641039885@163.com>
2022-08-07 15:35:33 +0800

    fix: 修复创建 ts 项目vue类型错误问题

--- template/common/sfc.d.ts
+++ template/common/sfc.d.ts
@@ -1,4 +1,7 @@
-import Vue from 'vue'
+declare module "*.vue" {
+  import Vue from 'vue'
+  export default Vue
+}
 
 declare module "vue/types/options" {
   type Hooks = App.AppInstance & Page.PageInstance;
