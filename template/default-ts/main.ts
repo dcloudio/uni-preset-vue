a4dacf3ed3bb10759fd0d2f725c178839d782e73
qiang <guoshengqiang@live.com>
2022-09-15 11:22:23 +0800

    fix: update App.vue fro ts template

--- template/default-ts/main.ts
+++ template/default-ts/main.ts
@@ -3,4 +3,5 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-new App().$mount()
+const app = new (typeof App === 'object' ? Vue.extend(Object.assign({ mpType: 'app' }, App)) : App)
+app.$mount();
