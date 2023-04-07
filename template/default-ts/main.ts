c8f557ec2313624002a1d349949c7ba2b296f1ee
DCloud_LXH <283700113@qq.com>
2023-04-07 15:33:24 +0800

    chore: uni.promisify.adaptor.js

--- template/default-ts/main.ts
+++ template/default-ts/main.ts
@@ -1,32 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './uni.promisify.adaptor'
 
 Vue.config.productionTip = false
-
-function isPromise(obj: any) {
-  return (
-    !!obj &&
-    (typeof obj === "object" || typeof obj === "function") &&
-    typeof obj.then === "function"
-  );
-}
-
-uni.addInterceptor({
-  returnValue(res: any) {
-    if (!isPromise(res)) {
-      return res;
-    }
-    return new Promise((resolve, reject) => {
-      res.then((res: [any, any]) => {
-        if (res[0]) {
-          reject(res[0]);
-        } else {
-          resolve(res[1]);
-        }
-      });
-    });
-  },
-});
 
 const app = new (typeof App === 'function' ? App : Vue.extend(Object.assign({ mpType: 'app' }, App)))
 app.$mount();
--- template/default/main.js
+++ template/default/main.js
@@ -1,34 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import './uni.promisify.adaptor'
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
-
-function isPromise(obj) {
-  return (
-    !!obj &&
-    (typeof obj === "object" || typeof obj === "function") &&
-    typeof obj.then === "function"
-  );
-}
-
-uni.addInterceptor({
-  returnValue(res) {
-    if (!isPromise(res)) {
-      return res;
-    }
-    return new Promise((resolve, reject) => {
-      res.then((res) => {
-        if (res[0]) {
-          reject(res[0]);
-        } else {
-          resolve(res[1]);
-        }
-      });
-    });
-  },
-});
 
 const app = new Vue({
   ...App
